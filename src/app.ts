afd27e491f6ac836a7784b591796bd90275549a6
Henry Chan <henry.chan.tii@gmail.com>
2026-06-26 12:13:40 -0400

    setup cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,56 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import fastifyEnv from '@fastify/env';
+
 import type { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import helloRoutes from './modules/hello/hello.routes';
 
-const buildApp = (opts = {}) => {
+const envSchema = {
+  type: 'object',
+  required: [ 'PORT' ],
+  properties: {
+    PORT: {
+      type: 'number',
+      default: 3000
+    },
+    HOST: {
+      type: 'string',
+      default: '0.0.0.0'
+    },
+    ALLOWED_ORIGINS: {
+      type: 'string'
+    }
+  }
+};
+declare module 'fastify' {
+  interface FastifyInstance {
+    config: { // this should be the same as the confKey in options
+      // specify your typing here
+      PORT: number,
+      HOST: string,
+      ALLOWED_ORIGINS?: string
+    };
+  }
+};
+
+const buildApp = async (opts = {}) => {
 	
   const app = Fastify(opts).withTypeProvider<TypeBoxTypeProvider>();
 
+
+  await app.register(fastifyEnv, {
+    schema: envSchema,
+    confKey: 'config',
+    dotenv: false
+  });
+
+  
+  await app.register(cors, {
+    origin: app?.config?.ALLOWED_ORIGINS?.split(',') ?? false
+  });
+
   // Register your modules
-  app.register(helloRoutes, { prefix: '/api/v1' });
+  await app.register(helloRoutes, { prefix: '/api/v1' });
 
   return app;
 };

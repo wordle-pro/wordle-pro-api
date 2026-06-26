8b3f22a28d2f3e168a80a9076bbe097eda4a4c8d
Henry Chan <hchan07@users.noreply.github.com>
2026-06-26 12:15:07 -0400

    setup cors (#2)

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import buildApp from './app.js';
 
 const start = async () => {
-  const app = buildApp({
+  const app = await buildApp({
     routerOptions: {
       ignoreTrailingSlash: true
-    }
+    },
+    logger: true, 
   });
   try {
-    await app.listen({ port: 3000, host: '0.0.0.0' });
+    await app.ready();
+    app.log.info('Server stated:');
+    app.log.info({...app.config, nodeEnv: process.env.NODE_ENV});    
+    await app.listen({ port: app.config.PORT, host: app.config.HOST });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
   }
 };
+
 start();
